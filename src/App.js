1aebff17465e430ad2ca1c1f47853ee4ac1a38a1
pranav k <pranavkkeloth@gmail.com>
2023-05-30 12:53:40 +0530

    gonres and video are added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import { action, originals, comedy, horror, romance, documentary} from './urls';
+
 
 
 function App() {
+  
   return (
     <div className="App">
      <NavBar/>
      <Banner/>
+     <RowPost url={originals} title='Netflix Originals'/>
+     <RowPost url={action} title='Action' isSmall/>
+     <RowPost url={comedy} title='Comedy' isSmall/>
+     <RowPost url={horror} title='Horror' isSmall/>
+     <RowPost url={romance} title='Romance' isSmall/>
+     <RowPost url={documentary} title='Documentary' isSmall/>
+    
+    
     </div>
   );
 }
